7f17c42fc11dae857815ecd7541763570639fc28
welliton gervickas <wellitogervickas@gmail.com>
2024-04-22 12:48:09 -0300

    feat: adding page cards

--- src/payload/plugins/core/fields/pages.ts
+++ src/payload/plugins/core/fields/pages.ts
@@ -1,5 +1,7 @@
 import type { CollectionConfig } from 'payload/types';
 
+import { map } from 'lodash';
+
 export enum PagesContext {
   Homepage,
 }
@@ -11,7 +13,96 @@ export type Pages = {
 
 export const pages = ({ fields, context }: Pages): CollectionConfig['fields'] => {
   if (context === PagesContext.Homepage) {
-    return [...fields];
+    return map(fields, field => {
+      if (field.type === `tabs`) {
+        return {
+          ...field,
+          tabs: map(field.tabs, tab => {
+            if (tab.label === `Content`) {
+              return {
+                ...tab,
+                fields: map(tab.fields, tabField => {
+                  if (tabField.type === `blocks`) {
+                    return {
+                      ...tabField,
+                      blocks: [
+                        ...tabField.blocks,
+                        {
+                          slug: `cards`,
+                          fields: [
+                            {
+                              name: `sectionID`,
+                              label: `Section ID`,
+                              type: `text`,
+                              validate: value => {
+                                if (value && !/^[\w-]+$/.test(value)) {
+                                  return `Field should only using alphanumeric and -`;
+                                }
+                                return true;
+                              },
+                            },
+                            {
+                              type: `array`,
+                              name: `cardsList`,
+                              label: `Cards`,
+                              labels: {
+                                singular: `Card`,
+                                plural: `Cards`,
+                              },
+                              fields: [
+                                {
+                                  type: `text`,
+                                  name: `title`,
+                                  label: `Title`,
+                                },
+                                {
+                                  type: `textarea`,
+                                  name: `description`,
+                                  label: `Description`,
+                                },
+                                {
+                                  type: `select`,
+                                  name: `cardVariant`,
+                                  label: `Card Variant`,
+                                  defaultValue: `default`,
+                                  admin: {
+                                    description: `Choose the variant for the card.`,
+                                  },
+                                  options: [
+                                    {
+                                      label: `Default`,
+                                      value: `default`,
+                                    },
+                                    {
+                                      label: `Primary`,
+                                      value: `primary`,
+                                    },
+                                    {
+                                      label: `Secondary`,
+                                      value: `secondary`,
+                                    },
+                                  ],
+                                },
+                              ],
+                            },
+                          ],
+                        },
+                      ],
+                    };
+                  }
+
+                  return tabField;
+                }),
+              };
+            }
+
+            return tab;
+          }),
+        };
+      }
+
+      return field;
+    });
   }
 
   return [...fields];
