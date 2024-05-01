c0bf8ab372c93ca126e86e8c616ce77792609271
welliton gervickas <wellitogervickas@gmail.com>
2024-05-01 13:14:31 -0300

    feat: leaderboard field

--- src/payload/blocks/leaderboard/leaderboard.ts
+++ src/payload/blocks/leaderboard/leaderboard.ts
@@ -12,5 +12,30 @@ export const leaderboard: Block = {
       type: `relationship`,
       relationTo: `rewards-program`,
     },
+    {
+      type: `select`,
+      name: `cardVariant`,
+      label: `Card Variant`,
+      defaultValue: `default`,
+      admin: {
+        description: `Choose the variant for the card.`,
+      },
+      options: [
+        {
+          label: `Default`,
+          value: `default`,
+        },
+
+        {
+          label: `Primary`,
+          value: `primary`,
+        },
+
+        {
+          label: `Secondary`,
+          value: `secondary`,
+        },
+      ],
+    },
   ],
 };
