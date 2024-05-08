47f2a3dcc3834ba9f65c5787b370e134657aeac3
welliton gervickas <wellitogervickas@gmail.com>
2024-05-08 20:24:23 -0300

    chore: remove comments

--- src/payload/plugins/owl-protocol/webhook/get-user.ts
+++ src/payload/plugins/owl-protocol/webhook/get-user.ts
@@ -1,7 +1,5 @@
 import type { Payload } from 'payload';
 
-// import APIError from 'payload/dist/errors/APIError';
-
 import { type Settings, SettingsKeys } from '../types';
 import { proxy } from './proxy';
 
@@ -42,38 +40,4 @@ export const getUser = async ({
     path: `/project/projectUser?${queryParams}`,
     payload,
   });
-
-  // const owlSettings = (await payload.findGlobal({
-  //   slug: `owlProtocol`,
-  // })) as Settings;
-
-  // const headers = new Headers();
-  // headers.set(`Accept`, `application/json`);
-  // headers.set(`content-type`, `application/json`);
-  // headers.set(`x-api-key`, `${owlSettings[SettingsKeys.XApiKey]}`);
-
-  // const queryParams = new URLSearchParams();
-  // queryParams.set(`email`, email);
-  // queryParams.set(`userId`, userId);
-  // queryParams.set(`externalId`, externalId);
-  // queryParams.set(`projectId`, `${owlSettings[SettingsKeys.ProjectId]}`);
-  // queryParams.set(`chainId`, `${owlSettings[SettingsKeys.ChainId]}`);
-  // queryParams.set(`authMode`, `project`);
-
-  // const result = await fetch(
-  //   `${owlSettings[SettingsKeys.API]}` + `/project/projectUser?${queryParams}`,
-  //   {
-  //     method: `GET`,
-  //     headers,
-  //   },
-  // );
-
-  // if (result.status !== 200) {
-  //   const msg = await result.text();
-  //   throw new APIError(msg, result.status);
-  // }
-
-  // const data = await result.json();
-
-  // return data;
 };
